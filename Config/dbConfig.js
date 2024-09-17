bbe006f8e62df9695b03d521b5094ab0a528bde9
Himanshu <himanshu.garg@orium.com>
2024-09-17 13:31:17 -0700

    Update dbConfig.js

--- Config/dbConfig.js
+++ Config/dbConfig.js
@@ -2,7 +2,7 @@
 if (process.env.NODE_ENV === 'dev') {
     exports.config = {
         PORT: 8000,
-        dbURI: 'mongodb://unet-dev-uSeR:3!unet!!9U2g65fsBNMV@45.232.252.32/unet-DB'
+        dbURI: ''
     }
 }
 else {
